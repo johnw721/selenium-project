7b55893f841e655717ce2f2ffb1d508bdffe1885
johnwalker721@gmail.com <johnwalker721@gmail.com>
2024-03-06 17:50:07 -0500

    added drivers

--- src/main/java/wright/jarel/Main.java
+++ src/main/java/wright/jarel/Main.java
@@ -3,5 +3,7 @@ package wright.jarel;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Grey\\Documents\\Software_Projects\\selenium project\\demo\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
     }
 }
