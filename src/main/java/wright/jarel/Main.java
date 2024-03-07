b1257758bbd54e2fdf7cd1a05deceb802562ec3f
johnwalker721@gmail.com <johnwalker721@gmail.com>
2024-03-06 19:14:22 -0500

    corrected the artifactId for selenium

--- src/main/java/wright/jarel/Main.java
+++ src/main/java/wright/jarel/Main.java
@@ -1,9 +1,19 @@
 package wright.jarel;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Grey\\Documents\\Software_Projects\\selenium project\\demo\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+
+        driver.open(args);
+        String title = driver.getTitle();
+
+        driver.stop();
+
+        System.out.println("Hello world!");
     }
 }
